39aa7d1a15d8fe580d1fae0cf7e27fbc57f6dc9a
Hasebe Masahiko <JCA03205@nifty.com>
2015-01-05 08:08:51 +0900

    I2C処理のライブラリを展開
    低レベルのアクセス関数を読み書きの関数内に展開。またデバッグ用にどのセンサにアクセスしているか分かる変数を追加。

--- i2cdevice.c
+++ i2cdevice.c
@@ -11,93 +11,6 @@
 #include <xc.h>
 
 //-------------------------------------------------------------------------
-//			I2C Basic Functions
-//-------------------------------------------------------------------------
-//#ifndef _XTAL_FREQ
-    /* 例：4MHzの場合、4000000 をセットする */
-//    #define _XTAL_FREQ 48000000
-//#endif
-
-bool i2cErr;
-#define	LED2         PORTCbits.RC5
-
-// I2C通信がビジー状態を脱するまで待つ
-void i2c_check(void){
-	volatile int cnt=0;
-    while ( ( SSPCON2 & 0x1F ) || ( SSPSTAT & 0x05 ) ){	//	Buffer Full Check
-		if ( cnt++ > 1000 ){ i2cErr = SSPCON2 & 0x1F? true:false; break;}
-	}
-}
-void i2c_wait(void){
-	volatile int cnt=0;
-    while ( ( SSPCON2 & 0x1F ) || ( SSPSTAT & 0x04 ) ){
-		if ( cnt++ > 1000 ){ i2cErr = SSPCON2 & 0x1F? true:false; break;}
-	}
-}
-
-// I2Cバスを有効化
-void i2c_enable(void)
-{
-    SSPSTAT = 0b10000000;      // I2C 100kHz
-    SSPADD = 0x77;             // I2Cbus Baud rate,  48MHz/((SSP1ADD + 1)*4) = 400kHz -> 0x1d, 100kHz -> 0x77
-    SSPCON1 = 0b00001000;      // I2C enable, Master Mode
-	SSPCON2 = 0x00;
-	i2cErr = false;
-	SSPCON1bits.SSPEN = 1;		//	I2C enable
-}
-
-// I2Cバスを無効化
-void i2c_disable(void){
-    SSPCON1bits.SSPEN = 0;      // I2C disable
-}
-
-// I2C書き込みサイクルの開始（Start Conditionの発行）
-void i2c_start(void){
-    i2c_check();
-	SSPCON2bits.SEN = 1;       //  Start Condition Enabled bit
-    i2c_check();
-}
-
-// I2C書き込みサイクルの開始（Repeat Start Conditionの発行）
-void i2c_repeat_start(void){
-    SSPCON2bits.RSEN = 1;      //  Start Condition Enabled bit
-    i2c_check();
-}
-
-// I2C書き込みサイクルの終了（Stop Conditionの発行）
-void i2c_stop(void){
-    i2c_check();
-	SSPCON2bits.PEN = 1;       // Stop Condition Enable bit
-}
-
-// I2Cバスにデータを送信（1バイト分）
-void i2c_send_byte(const unsigned char data){
-    //i2c_check();
-	SSPBUF = data;
-    i2c_check();
-	//	while(SSPCON2bits.ACKSTAT==1);
-}
-
-//I2Cバスからデータ受信
-// ack=1 : 受信後ACKを送信し、次のデータを送るようスレーブデバイスに指示
-// ack=0 : 受信後NO_ACKを送信し、これ以上受信しないことをスレーブデバイスに指示
-unsigned char i2c_read_byte(const char ack){
-    i2c_check();
-	SSPCON2bits.RCEN = 1;
-    i2c_wait();
-    unsigned char data = SSPBUF;
-    i2c_check();
-
-    if(ack) SSPCON2bits.ACKDT = 0;     // ACK
-    else SSPCON2bits.ACKDT = 1;        // NO_ACK
-
-    SSPCON2bits.ACKEN = 1;
-
-    i2c_check();
-    return data;
-}
-
-//-------------------------------------------------------------------------
 //			Constants
 //-------------------------------------------------------------------------
 //static unsigned char GPIO_EXPANDER_ADDRESS = 0x3e;
@@ -111,76 +24,223 @@ static unsigned char ACCEL_SENSOR_ADDRESS = 0x1d;
 // I2C Bus Control Definition
 #define I2C_WRITE_CMD 0
 #define I2C_READ_CMD 1
+
 //-------------------------------------------------------------------------
-//			I2c Device Access Functions
+//			I2C Basic Functions
+//-------------------------------------------------------------------------
+//#ifndef _XTAL_FREQ
+    /* 例：4MHzの場合、4000000 をセットする */
+//    #define _XTAL_FREQ 48000000
+//#endif
+
+bool i2cErr;
+unsigned char i2cAdrs;
 //-------------------------------------------------------------------------
 void initI2c( void )
 {
-	i2c_enable();
+    SSPSTAT = 0b00000000;      // I2C 400kHz
+    SSPADD = 0x1d;             // I2Cbus Baud rate,  48MHz/((SSP1ADD + 1)*4) = 400kHz -> 0x1d, 100kHz -> 0x77
+    SSPCON1 = 0b00001000;      // I2C enable, Master Mode
+	SSPCON2 = 0x00;
+	i2cErr = false;
+	i2cAdrs = 0;
+	SSPCON1bits.SSPEN = 1;		//	I2C enable
 }
 //-------------------------------------------------------------------------
 void quitI2c( void )
 {
-	i2c_disable();
+    SSPCON1bits.SSPEN = 0;      // I2C disable
 }
+//-------------------------------------------------------------------------
+// I2C通信がビジー状態を脱するまで待つ
+bool i2c_check(void){
+	volatile int cnt=0;
+    while ( ( SSPCON2 & 0x5F ) || ( SSPSTAT & 0x05 ) ){	//	Buffer Full Check
+		if ( cnt++ > 1000 ){
+			i2cErr = ((SSPCON2 & 0x18)&&(i2cAdrs == TOUCH_SENSOR_ADDRESS))? true:false;
+			return false;
+		}
+	}
+	return true;
+}
+//-------------------------------------------------------------------------
+bool i2c_wait(void){
+	volatile int cnt=0;
+    while ( ( SSPCON2 & 0x5F ) || ( SSPSTAT & 0x04 ) ){
+		if ( cnt++ > 1000 ){
+			i2cErr = ((SSPCON2 & 0x18)&&(i2cAdrs == TOUCH_SENSOR_ADDRESS))? true:false;
+			return false;
+		}
+	}
+	return true;
+}
+//-------------------------------------------------------------------------
+void i2c_err(void)
+{
+	SSPCON1bits.WCOL=0;
+	SSPCON2bits.PEN = 1;
+	while(SSPCON2bits.PEN);	
+}
+
+//-------------------------------------------------------------------------
+//			I2c Device Access Functions
 //-------------------------------------------------------------------------
 void writeI2c( unsigned char adrs, unsigned char data )
 {
-	i2c_start();
-	i2c_send_byte((adrs<<1) | I2C_WRITE_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Write=0）を付与
-	i2c_send_byte(data);
-	i2c_stop();
+	if ( i2c_check() == false ){i2c_err(); return;}
+
+	SSPCON2bits.SEN = 1;       //  Start Condition Enabled bit
+	while(SSPCON2bits.SEN);
+
+	SSPBUF = (adrs<<1) | I2C_WRITE_CMD;
+    if ( i2c_check() == false ){i2c_err(); return;}
+	//while(SSPCON2bits.ACKSTAT==1);
+
+	SSPBUF = data;
+    if ( i2c_check() == false ){i2c_err(); return;}
+	//while(SSPCON2bits.ACKSTAT==1);
+
+	SSPCON2bits.PEN = 1;       // Stop Condition Enable bit
+	while(SSPCON2bits.PEN);
+	
+//	i2c_start();
+//	i2c_send_byte((adrs<<1) | I2C_WRITE_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Write=0）を付与
+//	i2c_send_byte(data);
+//	i2c_stop();
 }
 //-------------------------------------------------------------------------
 void writeI2cWithCmd( unsigned char adrs, unsigned char cmd, unsigned char data )
 {
-	i2c_start();
-	i2c_send_byte((adrs<<1) | I2C_WRITE_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Write=0）を付与
-	i2c_send_byte(cmd);
-	i2c_send_byte(data);
-	i2c_stop();
+    if ( i2c_check() == false ){i2c_err(); return;}
+
+	SSPCON2bits.SEN = 1;       //  Start Condition Enabled bit
+	while(SSPCON2bits.SEN);
+
+	SSPBUF = (adrs<<1) | I2C_WRITE_CMD;
+    if ( i2c_check() == false ){i2c_err(); return;}
+	//while(SSPCON2bits.ACKSTAT==1);
+
+	SSPBUF = cmd;
+    if ( i2c_check() == false ){i2c_err(); return;}
+	//while(SSPCON2bits.ACKSTAT==1);
+
+	SSPBUF = data;
+    if ( i2c_check() == false ){i2c_err(); return;}
+	//while(SSPCON2bits.ACKSTAT==1);
+
+	SSPCON2bits.PEN = 1;       // Stop Condition Enable bit
+	while(SSPCON2bits.PEN);
+
+//	i2c_start();
+//	i2c_send_byte((adrs<<1) | I2C_WRITE_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Write=0）を付与
+//	i2c_send_byte(cmd);
+//	i2c_send_byte(data);
+//	i2c_stop();
 }
 //-------------------------------------------------------------------------
 void writeI2cWithCmdAndMultiData( unsigned char adrs, unsigned char cmd, unsigned char* data, int length )
 {
 	int i=0;
-	i2c_start();
-	i2c_send_byte((adrs<<1) | I2C_WRITE_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Write=0）を付与
-	i2c_send_byte(cmd);
+
+    if ( i2c_check() == false ){i2c_err(); return;}
+
+	SSPCON2bits.SEN = 1;       //  Start Condition Enabled bit
+	while(SSPCON2bits.SEN);
+
+	SSPBUF = (adrs<<1) | I2C_WRITE_CMD;
+    if ( i2c_check() == false ){i2c_err(); return;}
+	//while(SSPCON2bits.ACKSTAT==1);
+
+	SSPBUF = cmd;
+    if ( i2c_check() == false ){i2c_err(); return;}
+	//while(SSPCON2bits.ACKSTAT==1);
+
 	while (i<length){
-		i2c_send_byte(*(data+i));
+		SSPBUF = *(data+i);
+		if ( i2c_check() == false ){i2c_err(); return;}
+		//while(SSPCON2bits.ACKSTAT==1);
 		i++;
 	}
-	i2c_stop();
+
+	SSPCON2bits.PEN = 1;       // Stop Condition Enable bit
+	while(SSPCON2bits.PEN);
+
+//	i2c_start();
+//	i2c_send_byte((adrs<<1) | I2C_WRITE_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Write=0）を付与
+//	i2c_send_byte(cmd);
+//	while (i<length){
+//		i2c_send_byte(*(data+i));
+//		i++;
+//	}
+//	i2c_stop();
 }
 //-------------------------------------------------------------------------
-void readI2c( unsigned char adrs, unsigned char* data )
-{
-	i2c_start();
-	i2c_send_byte((adrs<<1) | I2C_READ_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Read=1）を付与
-	*data = i2c_read_byte(0);
-	i2c_stop();
-}
+//void readI2c( unsigned char adrs, unsigned char* data )
+//{
+//	i2c_start();
+//	i2c_send_byte((adrs<<1) | I2C_READ_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Read=1）を付与
+//	*data = i2c_read_byte(0);
+//	i2c_stop();
+//}
 //-------------------------------------------------------------------------
 void readI2cWithCmd( unsigned char adrs, unsigned char cmd, unsigned char* data, int length )
 {
 	int i=0;
-	i2c_start();
-	i2c_send_byte((adrs<<1) | I2C_WRITE_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Write=0）を付与
-	i2c_send_byte(cmd);
 
-	i2c_repeat_start();
-	i2c_send_byte((adrs<<1) | I2C_READ_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Read=1）を付与
+    if ( i2c_check() == false ){i2c_err(); return;}
+
+	SSPCON2bits.SEN = 1;       //  Start Condition Enabled bit
+	while(SSPCON2bits.SEN);
+
+	SSPBUF = (adrs<<1) | I2C_WRITE_CMD;
+    if ( i2c_check() == false ){i2c_err(); return;}
+	//while(SSPCON2bits.ACKSTAT==1);
+
+	SSPBUF = cmd;
+    if ( i2c_check() == false ){i2c_err(); return;}
+	//while(SSPCON2bits.ACKSTAT==1);
+
+    SSPCON2bits.RSEN = 1;      //  Start Condition Enabled bit
+	while(SSPCON2bits.RSEN);
+
+	SSPBUF = (adrs<<1) | I2C_READ_CMD;
+    if ( i2c_check() == false ){i2c_err(); return;}
+	//while(SSPCON2bits.ACKSTAT==1);
+
 	while (i<length){
-		if ( length > i+1 ){
-			*(data+i) = i2c_read_byte(1);
-		}
-		else {	//	Final
-			*(data+i) = i2c_read_byte(0);
-		}
+
+		if ( i2c_check() == false ){i2c_err(); return;}
+		SSPCON2bits.RCEN = 1;
+		if ( i2c_wait() == false ){i2c_err(); return;}
+		*(data+i) = SSPBUF;
+		if ( i2c_check() == false ){i2c_err(); return;}
+
+		if ( length > i+1 )	SSPCON2bits.ACKDT = 0;     // ACK
+		else				SSPCON2bits.ACKDT = 1;     // NO_ACK
+		SSPCON2bits.ACKEN = 1;
+		if ( i2c_check() == false ){i2c_err(); return;}
+
 		i++;
 	}
-	i2c_stop();
+
+	SSPCON2bits.PEN = 1;       // Stop Condition Enable bit
+	while(SSPCON2bits.PEN);
+
+//	i2c_start();
+//	i2c_send_byte((adrs<<1) | I2C_WRITE_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Write=0）を付与
+//	i2c_send_byte(cmd);
+//	i2c_repeat_start();
+//	i2c_send_byte((adrs<<1) | I2C_READ_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Read=1）を付与
+//	while (i<length){
+//		if ( length > i+1 ){
+//			*(data+i) = i2c_read_byte(1);
+//		}
+//		else {	//	Final
+//			*(data+i) = i2c_read_byte(0);
+//		}
+//		i++;
+//	}
+//	i2c_stop();
 }
 
 //-------------------------------------------------------------------------
@@ -201,6 +261,7 @@ void readI2cWithCmd( unsigned char adrs, unsigned char cmd, unsigned char* data,
 void LPS331AP_init( void )
 {
 	//	Init Parameter
+	i2cAdrs = PRESSURE_SENSOR_ADDRESS;
 	writeI2cWithCmd( PRESSURE_SENSOR_ADDRESS, PRES_SNCR_RESOLUTION, 0x6A );	//	Resolution
 	writeI2cWithCmd( PRESSURE_SENSOR_ADDRESS, PRES_SNCR_PWRON, 0xf0 );	//	Power On
 }
@@ -210,6 +271,7 @@ int LPS331AP_getPressure( void )
 	unsigned char	dt[3];
 	float	tmpPrs = 0;
 
+	i2cAdrs = PRESSURE_SENSOR_ADDRESS;
 	readI2cWithCmd( PRESSURE_SENSOR_ADDRESS, PRES_SNCR_DT_L|0x80, dt, 3 );
 	tmpPrs = (float)(((unsigned long)dt[2]<<16)|((unsigned long)dt[1]<<8)|dt[0]);
 	tmpPrs = tmpPrs*10/4096;
@@ -241,6 +303,7 @@ void MPR121_init( void )
 {
 	int	i, j;
 
+	i2cAdrs = TOUCH_SENSOR_ADDRESS;
 	//	Init Parameter
 	// Put the MPR into setup mode
 	writeI2cWithCmd( TOUCH_SENSOR_ADDRESS, TCH_SNCR_ELE_CFG, 0x00 );
@@ -312,6 +375,7 @@ unsigned char MPR121_getTchSwData( void )
 {
 	unsigned char buf[2];
 
+	i2cAdrs = TOUCH_SENSOR_ADDRESS;
 	readI2cWithCmd( TOUCH_SENSOR_ADDRESS, TCH_SNCR_TOUCH_STATUS1, buf, 2 );
 
 //	return (buf[1]<<8) | buf[0];
@@ -328,6 +392,7 @@ unsigned char MPR121_getTchSwData( void )
 void ADXL345_init( void )
 {
 	//	Start Access
+	i2cAdrs = ACCEL_SENSOR_ADDRESS;
 	writeI2cWithCmd(ACCEL_SENSOR_ADDRESS,ACCEL_SNCR_PWR_CTRL,0x08);			//	Start Measurement
 	writeI2cWithCmd(ACCEL_SENSOR_ADDRESS,ACCEL_SNCR_DATA_FORMAT,0x04);		//	Left Justified
 }
@@ -336,6 +401,8 @@ void ADXL345_getAccel( signed short* value )
 {
 	unsigned short tmp;
 	unsigned char reg[2];
+
+	i2cAdrs = ACCEL_SENSOR_ADDRESS;
 
 	readI2cWithCmd(ACCEL_SENSOR_ADDRESS,0x32,reg,2);
 	tmp = reg[0];
@@ -359,6 +426,8 @@ void ADXL345_getAccel( signed short* value )
 void BlinkM_init( void )
 {
 	unsigned char color[3] = {0x00,0x00,0x00};
+
+	i2cAdrs = LED_BLINKM_ADDRESS;
 	writeI2cWithCmd( LED_BLINKM_ADDRESS, 'o', 0 );
 	writeI2cWithCmd( LED_BLINKM_ADDRESS, 'f', 80 );
 	writeI2cWithCmdAndMultiData( LED_BLINKM_ADDRESS, 'n', color, 3 );
@@ -383,5 +452,6 @@ const unsigned char tNoteToColor[13][3] = {
 //-------------------------------------------------------------------------
 void BlinkM_changeColor( unsigned char note )
 {
+	i2cAdrs = LED_BLINKM_ADDRESS;
 	writeI2cWithCmdAndMultiData( LED_BLINKM_ADDRESS, 'c', (unsigned char*)tNoteToColor[note], 3 );
 }
